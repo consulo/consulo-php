d5d2df80dee1c7d271ef4590f6f936431f0c9768
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-04-16 00:45:18 +0300

    add production src support

---
+++ php-impl/src/main/java/consulo/php/module/PhpContentFolderSupportPatcher.java
@@ -0,0 +1,28 @@
+package consulo.php.module;
+
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+
+import com.intellij.openapi.roots.ModifiableRootModel;
+import consulo.php.module.extension.PhpModuleExtension;
+import consulo.roots.ContentFolderSupportPatcher;
+import consulo.roots.ContentFolderTypeProvider;
+import consulo.roots.impl.ProductionContentFolderTypeProvider;
+
+/**
+ * @author VISTALL
+ * @since 2019-04-16
+ */
+public class PhpContentFolderSupportPatcher implements ContentFolderSupportPatcher
+{
+	@Override
+	public void patch(@Nonnull ModifiableRootModel model, @Nonnull Set<ContentFolderTypeProvider> set)
+	{
+		PhpModuleExtension extension = model.getExtension(PhpModuleExtension.class);
+		if(extension != null)
+		{
+			set.add(ProductionContentFolderTypeProvider.getInstance());
+		}
+	}
+}
