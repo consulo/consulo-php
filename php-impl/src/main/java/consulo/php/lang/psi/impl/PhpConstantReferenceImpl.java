f13cf0d574a031a93e7ad024331da639b9095fc5
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-04-29 02:02:51 +0300

    caching for constant ref

--- php-impl/src/main/java/consulo/php/lang/psi/impl/PhpConstantReferenceImpl.java
+++ php-impl/src/main/java/consulo/php/lang/psi/impl/PhpConstantReferenceImpl.java
@@ -10,7 +10,10 @@ import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiPolyVariantReference;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.ResolveResult;
+import com.intellij.psi.impl.source.resolve.ResolveCache;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.IncorrectOperationException;
 import com.intellij.util.Processor;
@@ -25,14 +28,34 @@ import consulo.annotations.RequiredReadAction;
 import consulo.annotations.RequiredWriteAction;
 import consulo.php.completion.ClassUsageContext;
 import consulo.php.index.PhpDefineIndex;
+import consulo.php.lang.psi.resolve.PhpResolveResult;
 import consulo.php.lang.psi.visitors.PhpElementVisitor;
 
 /**
  * @author jay
- * @date Jun 30, 2008 1:44:07 AM
+ * @since Jun 30, 2008 1:44:07 AM
  */
-public class PhpConstantReferenceImpl extends PhpNamedElementImpl implements ConstantReference, PhpReferenceWithCompletion
+public class PhpConstantReferenceImpl extends PhpNamedElementImpl implements ConstantReference, PhpReferenceWithCompletion, PsiPolyVariantReference
 {
+	private static class OurResolver implements ResolveCache.PolyVariantResolver<PhpConstantReferenceImpl>
+	{
+		private static final OurResolver ourInstance = new OurResolver();
+
+		@Nonnull
+		@Override
+		public ResolveResult[] resolve(@Nonnull PhpConstantReferenceImpl constantReference, boolean incompleteCode)
+		{
+			Collection<PhpDefine> phpDefines = PhpDefineIndex.INSTANCE.get(constantReference.getName(), constantReference.getProject(), constantReference.getResolveScope());
+
+			PhpDefine define = ContainerUtil.getFirstItem(phpDefines);
+			if(define != null)
+			{
+				return new ResolveResult[]{new PhpResolveResult(define)};
+			}
+			return ResolveResult.EMPTY_ARRAY;
+		}
+	}
+
 	public PhpConstantReferenceImpl(ASTNode node)
 	{
 		super(node);
@@ -76,8 +99,16 @@ public class PhpConstantReferenceImpl extends PhpNamedElementImpl implements Con
 	@Nullable
 	public PsiElement resolve()
 	{
-		Collection<PhpDefine> phpDefines = PhpDefineIndex.INSTANCE.get(getName(), getProject(), getResolveScope());
-		return ContainerUtil.getFirstItem(phpDefines);
+		ResolveResult[] results = multiResolve(false);
+		return results.length == 1 ? results[0].getElement() : null;
+	}
+
+	@RequiredReadAction
+	@Nonnull
+	@Override
+	public ResolveResult[] multiResolve(boolean incompleteCode)
+	{
+		return ResolveCache.getInstance(getProject()).resolveWithCaching(this, OurResolver.ourInstance, true, incompleteCode);
 	}
 
 	@Override
@@ -131,7 +162,7 @@ public class PhpConstantReferenceImpl extends PhpNamedElementImpl implements Con
 	 *
 	 * @param newElementName the new name of the target element.
 	 * @return the new underlying element of the reference.
-	 * @throws com.intellij.util.IncorrectOperationException if the rename cannot be handled for some reason.
+	 * @throws IncorrectOperationException if the rename cannot be handled for some reason.
 	 */
 	@RequiredWriteAction
 	@Override
@@ -147,7 +178,7 @@ public class PhpConstantReferenceImpl extends PhpNamedElementImpl implements Con
 	 *
 	 * @param element the element which should become the target of the reference.
 	 * @return the new underlying element of the reference.
-	 * @throws com.intellij.util.IncorrectOperationException if the rebind cannot be handled for some reason.
+	 * @throws IncorrectOperationException if the rebind cannot be handled for some reason.
 	 */
 	@RequiredWriteAction
 	@Override
@@ -175,7 +206,7 @@ public class PhpConstantReferenceImpl extends PhpNamedElementImpl implements Con
 	 * @param name the new element name.
 	 * @return the element corresponding to this element after the rename (either <code>this</code>
 	 * or a different element if the rename caused the element to be replaced).
-	 * @throws com.intellij.util.IncorrectOperationException if the modification is not supported or not possible for some reason.
+	 * @throws IncorrectOperationException if the modification is not supported or not possible for some reason.
 	 */
 	@RequiredWriteAction
 	@Override
