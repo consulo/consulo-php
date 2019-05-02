a777864aff20bdda68f42ac934c770ef1ba0b283
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-05-03 00:53:01 +0300

    type resolving

--- php-impl/src/main/java/consulo/php/lang/psi/impl/PhpElvisExpressionImpl.java
+++ php-impl/src/main/java/consulo/php/lang/psi/impl/PhpElvisExpressionImpl.java
@@ -2,6 +2,10 @@ package consulo.php.lang.psi.impl;
 
 import javax.annotation.Nonnull;
 
+import com.jetbrains.php.lang.psi.elements.PhpExpression;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
+import com.jetbrains.php.lang.psi.elements.PhpReference;
+import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import consulo.php.lang.psi.PhpElvisExpression;
 import consulo.php.lang.psi.visitors.PhpElementVisitor;
 import com.intellij.lang.ASTNode;
@@ -21,5 +25,28 @@ public class PhpElvisExpressionImpl extends PhpElementImpl implements PhpElvisEx
 	public void accept(@Nonnull PhpElementVisitor visitor)
 	{
 		visitor.visitElvisExpression(this);
+	}
+
+	@Nonnull
+	@Override
+	public PhpType getType()
+	{
+		PhpPsiElement firstPsiChild = getFirstPsiChild();
+		if(firstPsiChild instanceof PhpReference)
+		{
+			PhpType type = ((PhpReference) firstPsiChild).resolveLocalType();
+			if(type != PhpType.EMPTY)
+			{
+				return type;
+			}
+		}
+
+		PhpExpression expression = findChildByClass(PhpExpression.class);
+		if(expression != null)
+		{
+			return expression.getType();
+		}
+
+		return PhpType.EMPTY;
 	}
 }
