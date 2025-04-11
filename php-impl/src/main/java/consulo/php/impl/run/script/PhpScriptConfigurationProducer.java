ab1d66c54788837fb3801a46230b36111f000cde
UNV <nikolay@yurchenko.su>
2025-04-11 10:45:40 +0300

    Reformatting PhpScriptConfigurationProducer.

--- php-impl/src/main/java/consulo/php/impl/run/script/PhpScriptConfigurationProducer.java
+++ php-impl/src/main/java/consulo/php/impl/run/script/PhpScriptConfigurationProducer.java
@@ -19,48 +19,44 @@ import consulo.virtualFileSystem.VirtualFile;
  * @since 2019-04-21
  */
 @ExtensionImpl
-public class PhpScriptConfigurationProducer extends RunConfigurationProducer<PhpScriptConfiguration>
-{
-	public PhpScriptConfigurationProducer()
-	{
-		super(PhpScriptConfigrationType.getInstance());
-	}
+public class PhpScriptConfigurationProducer extends RunConfigurationProducer<PhpScriptConfiguration> {
+    public PhpScriptConfigurationProducer() {
+        super(PhpScriptConfigrationType.getInstance());
+    }
 
-	@Override
-	protected boolean setupConfigurationFromContext(PhpScriptConfiguration configuration, ConfigurationContext context, Ref<PsiElement> sourceElement)
-	{
-		PhpFile phpFile = PsiTreeUtil.getParentOfType(sourceElement.get(), PhpFile.class, false);
-		if(phpFile == null)
-		{
-			return false;
-		}
-		PhpModuleExtension<?> extension = ModuleUtilCore.getExtension(phpFile, PhpModuleExtension.class);
-		if(extension == null)
-		{
-			return false;
-		}
+    @Override
+    protected boolean setupConfigurationFromContext(
+        PhpScriptConfiguration configuration,
+        ConfigurationContext context,
+        Ref<PsiElement> sourceElement
+    ) {
+        PhpFile phpFile = PsiTreeUtil.getParentOfType(sourceElement.get(), PhpFile.class, false);
+        if (phpFile == null) {
+            return false;
+        }
+        PhpModuleExtension<?> extension = ModuleUtilCore.getExtension(phpFile, PhpModuleExtension.class);
+        if (extension == null) {
+            return false;
+        }
 
-		configuration.setName(phpFile.getName());
-		configuration.SCRIPT_PATH = FileUtil.toSystemIndependentName(phpFile.getVirtualFile().getPath());
-		configuration.getConfigurationModule().setModule(extension.getModule());
-		return true;
-	}
+        configuration.setName(phpFile.getName());
+        configuration.SCRIPT_PATH = FileUtil.toSystemIndependentName(phpFile.getVirtualFile().getPath());
+        configuration.getConfigurationModule().setModule(extension.getModule());
+        return true;
+    }
 
-	@Override
-	public boolean isConfigurationFromContext(PhpScriptConfiguration configuration, ConfigurationContext context)
-	{
-		PhpFile phpFile = PsiTreeUtil.getParentOfType(context.getPsiLocation(), PhpFile.class, false);
-		if(phpFile == null)
-		{
-			return false;
-		}
+    @Override
+    public boolean isConfigurationFromContext(PhpScriptConfiguration configuration, ConfigurationContext context) {
+        PhpFile phpFile = PsiTreeUtil.getParentOfType(context.getPsiLocation(), PhpFile.class, false);
+        if (phpFile == null) {
+            return false;
+        }
 
-		VirtualFile file = LocalFileSystem.getInstance().findFileByPath(configuration.SCRIPT_PATH);
-		if(file == null)
-		{
-			return false;
-		}
+        VirtualFile file = LocalFileSystem.getInstance().findFileByPath(configuration.SCRIPT_PATH);
+        if (file == null) {
+            return false;
+        }
 
-		return Comparing.equal(file, phpFile.getVirtualFile());
-	}
+        return Comparing.equal(file, phpFile.getVirtualFile());
+    }
 }
