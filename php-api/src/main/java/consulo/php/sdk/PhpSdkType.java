1a1f6426b6bf5198c43703739ef6fd6a4fc5364d
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-20 16:53:32 +0400

    drop usage EP_NAME

--- php-api/src/main/java/consulo/php/sdk/PhpSdkType.java
+++ php-api/src/main/java/consulo/php/sdk/PhpSdkType.java
@@ -1,6 +1,7 @@
 package consulo.php.sdk;
 
 import consulo.annotation.component.ExtensionImpl;
+import consulo.application.Application;
 import consulo.container.plugin.PluginManager;
 import consulo.content.OrderRootType;
 import consulo.content.base.BinariesOrderRootType;
@@ -19,9 +20,9 @@ import consulo.process.util.ProcessOutput;
 import consulo.ui.image.Image;
 import consulo.virtualFileSystem.LocalFileSystem;
 import consulo.virtualFileSystem.VirtualFile;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -32,113 +33,95 @@ import java.util.List;
  * @author Maxim
  */
 @ExtensionImpl
-public class PhpSdkType extends SdkType
-{
-	private static final Logger LOGGER = Logger.getInstance(PhpSdkType.class);
+public class PhpSdkType extends SdkType {
+    private static final Logger LOGGER = Logger.getInstance(PhpSdkType.class);
 
-	@Nonnull
-	public static PhpSdkType getInstance()
-	{
-		return EP_NAME.findExtensionOrFail(PhpSdkType.class);
-	}
+    @Nonnull
+    public static PhpSdkType getInstance() {
+        return Application.get().getExtensionPoint(SdkType.class).findExtensionOrFail(PhpSdkType.class);
+    }
 
-	@Nonnull
-	public static String getExecutableFile(String home)
-	{
-		return home + File.separator + (Platform.current().os().isWindows() ? "php.exe" : "php");
-	}
+    @Nonnull
+    public static String getExecutableFile(String home) {
+        return home + File.separator + (Platform.current().os().isWindows() ? "php.exe" : "php");
+    }
 
-	public PhpSdkType()
-	{
-		super("PHP SDK");
-	}
+    public PhpSdkType() {
+        super("PHP SDK");
+    }
 
-	@Nonnull
-	@Override
-	public Collection<String> suggestHomePaths()
-	{
-		if(Platform.current().os().isMac())
-		{
-			return Collections.singletonList("/usr/bin");
-		}
-		return Collections.emptyList();
-	}
+    @Nonnull
+    @Override
+    public Collection<String> suggestHomePaths() {
+        if (Platform.current().os().isMac()) {
+            return Collections.singletonList("/usr/bin");
+        }
+        return Collections.emptyList();
+    }
 
-	@Override
-	public boolean isValidSdkHome(String path)
-	{
-		return new File(getExecutableFile(path)).exists();
-	}
+    @Override
+    public boolean isValidSdkHome(String path) {
+        return new File(getExecutableFile(path)).exists();
+    }
 
-	public static String getVersion(String home)
-	{
-		List<String> args = new ArrayList<>(2);
-		args.add(getExecutableFile(home));
-		args.add("--version");
-		try
-		{
-			ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(new GeneralCommandLine(args).withWorkDirectory(home));
-			List<String> stdoutLines = processOutput.getStdoutLines();
-			return !stdoutLines.isEmpty() ? stdoutLines.get(0) : null;
-		}
-		catch(ExecutionException e)
-		{
-			return null;
-		}
-	}
+    public static String getVersion(String home) {
+        List<String> args = new ArrayList<>(2);
+        args.add(getExecutableFile(home));
+        args.add("--version");
+        try {
+            ProcessOutput processOutput = CapturingProcessUtil.execAndGetOutput(new GeneralCommandLine(args).withWorkDirectory(home));
+            List<String> stdoutLines = processOutput.getStdoutLines();
+            return !stdoutLines.isEmpty() ? stdoutLines.get(0) : null;
+        }
+        catch (ExecutionException e) {
+            return null;
+        }
+    }
 
-	@Nullable
-	@Override
-	public String getVersionString(String s)
-	{
-		return getVersion(s);
-	}
+    @Nullable
+    @Override
+    public String getVersionString(String s) {
+        return getVersion(s);
+    }
 
-	@Override
-	public String suggestSdkName(String currentSdkName, String sdkHome)
-	{
-		return PhpBundle.message("default.php.sdk.name");
-	}
+    @Override
+    public String suggestSdkName(String currentSdkName, String sdkHome) {
+        return PhpBundle.message("default.php.sdk.name");
+    }
 
-	@Override
-	@Nonnull
-	public String getPresentableName()
-	{
-		return PhpBundle.message("php.sdk.type.name");
-	}
+    @Override
+    @Nonnull
+    public String getPresentableName() {
+        return PhpBundle.message("php.sdk.type.name");
+    }
 
-	@Override
-	public void setupSdkPaths(Sdk sdk)
-	{
-		final SdkModificator sdkModificator = sdk.getSdkModificator();
+    @Override
+    public void setupSdkPaths(Sdk sdk) {
+        final SdkModificator sdkModificator = sdk.getSdkModificator();
 
-		File pluginPath = PluginManager.getPluginPath(PhpSdkType.class);
+        File pluginPath = PluginManager.getPluginPath(PhpSdkType.class);
 
-		File stubDirectory = new File(pluginPath, "php-runtime-stubs");
-		VirtualFile stubsDir = LocalFileSystem.getInstance().findFileByIoFile(stubDirectory);
+        File stubDirectory = new File(pluginPath, "php-runtime-stubs");
+        VirtualFile stubsDir = LocalFileSystem.getInstance().findFileByIoFile(stubDirectory);
 
-		if(stubsDir == null)
-		{
-			LOGGER.warn("Cant find stubs for path: " + stubDirectory);
-		}
-		else
-		{
-			sdkModificator.addRoot(stubsDir, BinariesOrderRootType.getInstance());
-			sdkModificator.addRoot(stubsDir, SourcesOrderRootType.getInstance());
-		}
+        if (stubsDir == null) {
+            LOGGER.warn("Cant find stubs for path: " + stubDirectory);
+        }
+        else {
+            sdkModificator.addRoot(stubsDir, BinariesOrderRootType.getInstance());
+            sdkModificator.addRoot(stubsDir, SourcesOrderRootType.getInstance());
+        }
 
-		sdkModificator.commitChanges();
-	}
+        sdkModificator.commitChanges();
+    }
 
-	@Override
-	public boolean isRootTypeApplicable(OrderRootType type)
-	{
-		return type == BinariesOrderRootType.getInstance() || type == SourcesOrderRootType.getInstance();
-	}
+    @Override
+    public boolean isRootTypeApplicable(OrderRootType type) {
+        return type == BinariesOrderRootType.getInstance() || type == SourcesOrderRootType.getInstance();
+    }
 
-	@Override
-	public Image getIcon()
-	{
-		return PhpIconGroup.filetypesPhp();
-	}
+    @Override
+    public Image getIcon() {
+        return PhpIconGroup.filetypesPhp();
+    }
 }
